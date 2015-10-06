71dfc0223053c581e3fa105f99f21f96246ce5c6
ktc <ktc@statsbiblioteket.dk>
2015-10-06 09:32:27 +0200

    link start/stop/status scripts to karaf

--- sb_scripts/deploy_karaf.sh
+++ sb_scripts/deploy_karaf.sh
@@ -18,5 +18,8 @@ tar xf apache-karaf*
 cd apache-karaf*
 cp -r * $KARAF_DIR
 
-
+cd $HOME/bin 
+ln -sf $KARAF_DIR/bin/start karaf-start
+ln -sf $KARAF_DIR/bin/stop karaf-stop
+ln -sf $KARAF_DIR/bin/status karaf-status
 
