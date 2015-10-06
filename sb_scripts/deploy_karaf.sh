891c9bf2f7d32faf374f226826faf4fcc770756a
ktc <ktc@statsbiblioteket.dk>
2015-10-06 09:38:11 +0200

    don't make symlinks, just start karaf

--- sb_scripts/deploy_karaf.sh
+++ sb_scripts/deploy_karaf.sh
@@ -18,8 +18,4 @@ tar xf apache-karaf*
 cd apache-karaf*
 cp -r * $KARAF_DIR
 
-cd $HOME/bin 
-ln -sf $KARAF_DIR/bin/start karaf-start
-ln -sf $KARAF_DIR/bin/stop karaf-stop
-ln -sf $KARAF_DIR/bin/status karaf-status
-
+$KARAF_DIR/bin/start
